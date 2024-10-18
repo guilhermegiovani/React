52f5cc99e6f6a0ee69917c19827fe222812bdeab
Guilherme Nobre <146262450+guilhermegiovani@users.noreply.github.com>
2024-10-18 17:58:34 -0300

    Todo list
    todo

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -10,8 +10,59 @@ function App() {
 
   const [title, setTitle] = useState("")
   const [time, setTime] = useState("")
-  const [todo, setTodo] = useState([])
+  const [todos, setTodos] = useState([])
   const [loading, setLoading] = useState(false)
+
+  // Load todos on page load
+  useEffect(() => {
+
+    const loadData = async () => {
+
+      setLoading(true)
+
+      const res = await fetch(API + "/todos")
+      .then((res) => res.json())
+      .then((data) => data)
+      .catch((err) => console.log(err))
+
+      setLoading(false)
+
+      setTodos(res)
+
+    }
+
+    loadData()
+
+  }, [])
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    
+    const todo = {
+      id: Math.random(),
+      title,
+      time,
+      done: false,
+    }
+
+    await fetch(API + "/todos", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(todo)
+    })
+
+    setTodos((prevState) => [...prevState, todo])
+
+    setTitle("")
+    setTime("")
+
+  }
+
+  if(loading) {
+    return <p>Carregando...</p>
+  }
 
   return (
     <div className="App">
@@ -20,11 +71,47 @@ function App() {
       </div>
 
       <div className="form-todo">
-        <p>Formulário</p>
+        <h2>Insira a sua próxima tarefa:</h2>
+
+        <form onSubmit={handleSubmit}>
+
+          <div className="form-control">
+            <label htmlFor="title">O que você vai fazer?</label>
+            <input
+              type="text"
+              name='title'
+              placeholder='Título da tarefa'
+              onChange={(e) => setTitle(e.target.value)}
+              value={title || ""}
+              required
+            />
+          </div>
+
+          <div className="form-control">
+            <label htmlFor="time">Duração:</label>
+            <input
+              type="text"
+              name='time'
+              placeholder='Tempo estimado (em horas)'
+              onChange={(e) => setTime(e.target.value)}
+              value={time || ""}
+              required
+            />
+          </div>
+
+          <input type="submit" value="Criar Tarefa" />
+        </form>
       </div>
 
       <div className="list-todo">
-        <p>Lista</p>
+        <h2>Lista de tarefas:</h2>
+        {todos.length === 0 && <p>Não há tarefas!</p>}
+
+        {todos.map((todo) => (
+          <div className="todo" key={todo.id}>
+            <p>{todo.title}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
